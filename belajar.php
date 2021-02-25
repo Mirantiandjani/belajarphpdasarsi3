e98b2f2c09294ca405a5982a333631d2817a5691
Mirantiandjani <miranti.and@gmail.com>
2021-02-25 15:25:08 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nama = 'Miranti';
+    $angka = 12;
+    $angka_desimal = 12.7;
+
+    echo $nama;
+    echo '<br>'
+    echo $angka;
+
+    echo 'Hello Word!';
+
+    echo '<h1>hello world</1>';
+?>
